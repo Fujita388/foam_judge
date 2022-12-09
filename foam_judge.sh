ab6ce55cbe1c1f5ead03529cccdd6576545b9e1e
N.Fujita <naofuji.1220@gmail.com>
2022-12-09 19:33:40 +0900

    modify

--- foam_judge.sh
+++ foam_judge.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-#PBS -q i2cpu
-#PBS -l select=2:ncpus=128:mpiprocs=128:ompthreads=1
-#PBS -l walltime=00:30:00
+#PBS -q F16cpu
+#PBS -l select=16:ncpus=128:mpiprocs=128:ompthreads=1
+#PBS -l walltime=04:30:00
 #PBS -N foam_judge
 
 module load intel intel-mpi
-mpiexec -n 256 ./a.out > foam_judge.log 2>&1
+mpiexec -n 2048 ./a.out > foam_judge.log 2>&1
