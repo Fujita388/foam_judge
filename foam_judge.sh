499c1b4efe0eec325e04be8c8fd9ec222f4e7526
N.Fujita <naofuji.1220@gmail.com>
2022-12-18 16:39:06 +0900

    delete *.sh

--- foam_judge.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-#PBS -q F16cpu
-#PBS -l select=16:ncpus=128:mpiprocs=128:ompthreads=1
-#PBS -l walltime=04:30:00
-#PBS -N foam_judge
-
-module load intel intel-mpi
-mpiexec -n 2048 ./a.out > foam_judge.log 2>&1
